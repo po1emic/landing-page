4e2f096e397bb3d8d70f8026fca2ceab575e6d8a
den.riabchenkov@gmail.com <den.riabchenkov@gmail.com>
2024-12-22 15:37:27 +0300

    extract close menu function

--- src/js/index.js
+++ src/js/index.js
@@ -3,25 +3,20 @@ import "../styles/index.css";
 document.addEventListener("DOMContentLoaded", () => {
   const menuFrame = document.querySelector("#menu-frame");
 
+  const closeMenu = () => {
+    menuFrame.classList.remove("menu-active");
+    document.body.classList.remove("menu-body");
+  }
+
   document.querySelector("#menu-button").addEventListener("click", () => {
     menuFrame.classList.add("menu-active");
     document.body.classList.add("menu-body");
   });
 
-  document.querySelector("#menu-close").addEventListener("click", () => {
-    menuFrame.classList.remove("menu-active");
-    document.body.classList.remove("menu-body");
-  });
-
-  document.querySelector("#menu-overlay").addEventListener("click", () => {
-    menuFrame.classList.remove("menu-active");
-    document.body.classList.remove("menu-body");
-  });
-
+  document.querySelector("#menu-close").addEventListener("click", closeMenu);
+  document.querySelector("#menu-overlay").addEventListener("click", closeMenu);
+  
   document.querySelectorAll(".menu-link").forEach((link) => {
-    link.addEventListener("click", () => {
-      menuFrame.classList.remove("menu-active");
-      document.body.classList.remove("menu-body");
-    });
+    link.addEventListener("click", closeMenu);
   });
 });
